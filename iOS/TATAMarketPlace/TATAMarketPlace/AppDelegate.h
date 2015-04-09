1a1d913deb9bcd7cc5ef22819017614c8820a539
Kapil <kapil@Kapil.local>
2015-04-09 18:21:24 +0530

    adding iOS folder

---
+++ iOS/TATAMarketPlace/TATAMarketPlace/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TATAMarketPlace
+//
+//  Created by Kapil on 09/04/15.
+//  Copyright (c) 2015 TATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iOS/TATAMarketPlace/TATAMarketPlaceTests/TATAMarketPlaceTests.m
@@ -0,0 +1,40 @@
+//
+//  TATAMarketPlaceTests.m
+//  TATAMarketPlaceTests
+//
+//  Created by Kapil on 09/04/15.
+//  Copyright (c) 2015 TATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TATAMarketPlaceTests : XCTestCase
+
+@end
+
+@implementation TATAMarketPlaceTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
